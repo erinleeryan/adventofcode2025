ed37c71f38d75c231aea1894ad7aab7ec197d500
erinleeryan <erinleeryan@gmail.com>
2025-12-02 10:06:57 -0500

    Day 2 Part 1 is done
    Had to add the csv parsing to data parsing

--- code/data_parsing.py
+++ code/data_parsing.py
@@ -32,3 +32,27 @@ def read_2dmap_to_numpy(inputfile):
     map2d = np.array(map2d)
     return map2d
     
+def read_a_csv(inputfile, output='string', reduce=True):
+    # output means you can force things to be a string
+    # reduce means that rather than return a list of lists 
+    # where each constituent list represents a line
+    # you reduce something that is all smashed together in a single list
+
+    f = open("../data/"+inputfile, "r")
+    data = f.read().splitlines()
+    output_data = []
+    for line in data:
+        strings = line.split(',')
+        # remove trailing comma ends and any blank entries in the middle that might show up
+        strings = [item for item in strings if item != '']
+        if output == 'int':
+            strings = [int(x) for x in strings]
+        output_data.append(strings)
+
+    if reduce == True:
+        output_data = sum(output_data, [])
+
+
+    return output_data
+
+    
